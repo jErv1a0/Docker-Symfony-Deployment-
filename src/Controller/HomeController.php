d135effcf9e94c13cfc1942bb08633ef595b9f7c
jErv1a0 <alvrcoqviermv05@gmail.com>
2026-05-18 21:42:03 +0800

    Make homepage independent of doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(): Response
     {
-        $dbWorking = false;
-        try {
-            $conn = $em->getConnection();
-            // Run a simple query to check DB connection
-            $conn->executeQuery('SELECT 1');
-            $dbWorking = true;
-        } catch (\Exception $e) {
-            $dbWorking = false;
-        }
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'db_working' => $dbWorking,
         ]);
     }
 }
